8cb234ee3ef3940679fae4d7727b5c408d02f259
Robin Collins <info@robincollins.co.uk>
2020-08-03 10:02:16 +0000

    added registration functionality

--- app.py
+++ app.py
@@ -20,18 +20,37 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-@app.route('/')
+@app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.chew_tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exsists in db
+        exsisting_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if exsisting_user:
+            flash("Username already exsists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # Add new user into a session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP', "0.0.0.0"),
-            port=int(os.environ.get('PORT', "8080")),
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP", "0.0.0.0"),
+            port=int(os.environ.get("PORT", "8080")),
             debug=True)
